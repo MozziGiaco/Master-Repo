06485a994f8ba75a4b76a34c2c7a0af8a40b95ae
Hailey.forar <Hailey.forar@MU-C1-01.mullen.pvt.k12.co.us>
2020-01-09 11:23:07 -0700

    end of tips and tricks added to additional class

--- Boldness/src/Additional.java
+++ Boldness/src/Additional.java
@@ -8,6 +8,10 @@ public class Additional {
 	  System.out.println("Make sure to hold your head high when you enter a room!");
 	  
 	  System.out.println("Even if you don't have all the confidence in the world, pretend you do... and before long, you will!!");
+	  
+	  System.out.println("Make sure to dress to impress, but also dress to ensure that you feel good!");
+	  
+	  System.out.println("Take these few tips and tricks to BE BOLD!!");
 	}
 
 }
