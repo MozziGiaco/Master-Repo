2a1f81669cd2588fa091a5ee3113331e66c10f72
Hailey.forar <Hailey.forar@MU-C1-01.mullen.pvt.k12.co.us>
2020-01-09 11:17:59 -0700

    additional add-ons to additional class

--- Boldness/src/Additional.java
+++ Boldness/src/Additional.java
@@ -4,6 +4,10 @@ public class Additional {
 	public static void main(String[] args) 
 	{
 	  System.out.println("Here are additional tips for how to BE BOLD!!");
+	  
+	  System.out.println("Make sure to hold your head high when you enter a room!");
+	  
+	  System.out.println("Even if you don't have all the confidence in the world, pretend you do... and before long, you will!!");
 	}
 
 }
