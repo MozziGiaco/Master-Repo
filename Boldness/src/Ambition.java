b280dbdba1b049c11cdc2ff738f43e0f2f024060
Nicholas Giacomozzi <nicholas.giacomozzi@mullen.pvt.k12.co.us>
2020-01-09 11:41:18 -0700

    Added results of being ambitious

--- Boldness/src/Ambition.java
+++ Boldness/src/Ambition.java
@@ -5,6 +5,10 @@ public class Ambition
 		public static void main(String[] args)
 			{
 				System.out.println("BE AMBITIOUS");
+
+				System.out.println("Being ambitious allows you to do what you want in life!");
+
+				System.out.println("Being ambitious allows for greater confidence!");
 			}
 
 	}
